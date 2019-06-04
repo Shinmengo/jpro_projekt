0bf0124336f827be838dd38aef52623838bf4660
Patryk Adrian Mruk <296775@ameryk.fizyka.umk.pl>
2019-06-05 00:48:01 +0200

    Commit 9
    Dodanie funkcji step ktora sprawdza w kazdej turze czy komorki sa zywe (#) czy martwe (.)
    Poprawa paru bledow + lekka optymalizacja
    
    Do zrobienia:
    -blad przy wypisywaniu, nie wypisywane sa zadne kroki poza 1
    -kompiluje tylko na windowsie
    -dodanie linuxowej wersji programu

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 void begin(int rows, int columns);
 char *create(int rows, int columns);
 double randomValue(double min, double max);
 void display(int rows, int columns, char *simulation);
 int countNeighbors(int rows, int columns, int x, int y, char *simulation);
+char *step(int rows, int columns, char *previousSimulation);
 
 int main()
 {
 	int rows, columns;
 	printf("Podaj ilosc wierszy i kolumn\n");
 	scanf("%d %d", &rows, &columns);
+	printf("\n\n");
 	if(rows <= 0){
-		printf("Liczba wierszy musi byc wieksza od 0!\m");
+		printf("Liczba wierszy musi byc wieksza od 0!\n");
 		return -1;
 	}
 	rows+=2;
@@ -28,7 +31,6 @@ int main()
 }
 
 void display(int rows, int columns, char *simulation){
-	printf("\n\n\n\n\n");
 	int x,y;
 	for(y=1;y<=rows-1;y++){
 		for(x=1;x<=columns-1;x++){
@@ -45,11 +47,31 @@ int countNeighbors(int rows, int columns, int x, int y, char *simulation){
 	int i,j;
 	for(i=-1;i<=1;i++){
 		for(j=-1;j<=1;j++){
-			char c = *(simulation + pivot + (-1)*(columns-)1);
+			char c = *(simulation + pivot + (-1)*(columns-1));
 			if(c == '#') count++;
 		}
 	}
 	return count;
+}
+
+char *step(int rows, int columns, char *previousSimulation){
+	char *steppedSimulation = calloc(rows*columns, sizeof(int));
+	if(steppedSimulation == NULL) return NULL;
+	int x,y;
+	
+	for(y=1;y<=rows-1;y++){
+		for(x=1;x<=columns-1;x++){
+			int live = countNeighbors(rows, columns, x, y, previousSimulation);
+			char cell = *(previousSimulation + y*columns + x);
+			if(cell == '#') live--;
+			*(steppedSimulation + y*columns + x) == cell;
+			if(live < 2) *(previousSimulation + y*columns + x) = '.';
+			else if((live == 2 || live == 3) && cell == '#') *(previousSimulation + y*columns + x) == '#';
+			else if(live > 3 && cell == '#') *(previousSimulation + y*columns + x) == '.';
+			else if(live == 3 && cell == '.') *(previousSimulation + y*columns + x) == '#';
+		}
+	}
+	return steppedSimulation;
 }
 
 void begin(int rows, int columns){
@@ -62,6 +84,7 @@ void begin(int rows, int columns){
 		if(newSim == NULL) return;
 		free(simulation);
 		simulation = newSim;
+		printf("\n\n\n\n");
 		display(rows, columns, simulation);
 		Sleep(1500); //windows
 		//usleep(15000000); //linux
@@ -69,7 +92,7 @@ void begin(int rows, int columns){
 }
 
 double randomValue(double min, double max){
-	return ((double)rand()/RAND_MAX)*(max-min)+min);
+	return ((double)rand()/RAND_MAX)*(max-min)+min;
 }
 
 char *create(int rows, int columns){
