fe9ea2e73366416976751abce6113705b4a77b43
Patryk Adrian Mruk <296775@ameryk.fizyka.umk.pl>
2019-06-05 00:13:47 +0200

    Commit 5
    Dodanie funkcji display wypisujacej na ekran symulacje gry o zycie

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 void begin(int rows, int columns);
 char *create(int rows, int columns);
 double randomValue(double min, double max);
+void display(int rows, int columns, char *simulation);
 
 int main()
 {
@@ -23,6 +24,17 @@ int main()
 	columns+=2;
 
 	begin(rows, columns);
+}
+
+void display(int rows, int columns, char *simulation){
+	printf("\n\n\n\n\n");
+	int x,y;
+	for(y=1;y<=rows-1;y++){
+		for(x=1;x<=columns-1;x++){
+			printf("%c", *(simulation + y*columns + x));
+		}
+		printf("\n");
+	}
 }
 
 void begin(int rows, int columns){
@@ -49,7 +61,7 @@ char *create(int rows, int columns){
 	char *simulation = (char*)calloc(rows*columns, sizeof(char));
 	if(simulation == NULL) return;
 	int x,y;
-	for(y=1; y<rows-1;y++){
+	for(y=1;y<rows-1;y++){
 		for(x=1;x<columns-1;x++){
 			if(randomValue(0.1, 1.1) <= 0.35){
 				*(simulation + y*columns + x) = '#';
